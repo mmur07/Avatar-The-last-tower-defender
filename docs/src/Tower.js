6150a8bf150e5296128969390573e326e0d93ca1
mmur07 <mmur@ucm.es>
2019-10-19 17:31:24 +0200

    Mejorado el script torre y arreglado el script game

--- docs/src/Tower.js
+++ docs/src/Tower.js
@@ -4,9 +4,11 @@ export default class Tower extends Elemental{
 
     constructor(scene, element, xPos, yPos, range, cdShoots){
         super(scene, element, xPos, yPos);
-        this._range = range;
         this._cdShoots = cdShoots;
         //this._spriteName = spriteName;
+        this.aggroZone = new Phaser.GameObjects.Zone(scene, xPos, yPos);
+        this.aggroZone.setCircleDropZone(range);
+        
 
         this.scene.add.existing(this);
     }
@@ -14,5 +16,15 @@ export default class Tower extends Elemental{
     changeColor() {
         super.element = (super.element + 1) % 3;
         console.log(super.element);
+    }
+
+    update(time, delta){
+        
+    }
+}
+
+Tower.prototype.checkEnemies = (target) => {
+    if (target){
+
     }
 }
--- docs/src/game.js
+++ docs/src/game.js
@@ -8,15 +8,12 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {  
-    let jojoBG =  this.load.image('jojoBG','./img/thunderSplit.png');
     this.load.image('jojoSprite','./img/jojoSprite.png');
   }
 
   create() {
-    let bg = this.add.image(700,400,'jojoBG');
-    bg.setScale(.7);
     let DIO = new Tower(this,elements.FIRE, 50, 50, 20, 1, 'jojoSprite');
-    console.log(mul);
+    DIO.setScale(.2);
   }
 
   update(time, delta) {    

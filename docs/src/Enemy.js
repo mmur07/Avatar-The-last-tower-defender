26748c75d961750fd86370864916deb3193173d8
Adrian Alvarez <adralv01@ucm.es>
2019-11-03 11:54:48 +0100

    Pooling Poolido

--- docs/src/Enemy.js
+++ docs/src/Enemy.js
@@ -1,34 +1,41 @@
 import Elemental from "./Elemental.js";
 
-export default class Enemy extends Elemental{
-    constructor(scene,spritename,element,xPos,yPos,hp,speed){
-        super(scene,spritename,element,xPos,yPos)
+export default class Enemy extends Elemental {
+    constructor(scene, spritename, element, xPos, yPos, hp, speed) {
+        super(scene, spritename, element, xPos, yPos)
         this._speed = speed;
         this._hp = hp;
         this.scene.add.existing(this);
         this.setScale(0.5);
     }
-    get hp(){
+    get hp() {
         return this._hp;
     }
-}
-Enemy.prototype.ReceiveDMG = function (dmg,dmgType) {
-    this._hp = this._hp - (this.dmgMultiplier(dmgType)*dmg);
-    if(this._hp <=0 ){
-        this.die();
+
+    update() {
+
     }
-    console.log(this._hp);
-}
-Enemy.prototype.update = function () {
-    
-}
-Enemy.prototype.spawn = function (xPos,yPos) {
-    this.setActive(true);
-    this.setVisible(true);
-    this.setPosition(xPos,yPos);
-}
-Enemy.prototype.die = function(){
-    this.scene.ActiveEnemies.killAndHide(this);
-    this.scene.EnemyPool.add(this);
-    console.log("MUERO, quedan " + this.scene.ActiveEnemies.getLength()+ "enemigos");
+
+    ReceiveDMG(dmg, dmgType) {
+        this._hp = this._hp - (this.dmgMultiplier(dmgType) * dmg);
+        if (this._hp <= 0) {
+            this.die();
+        }
+    }
+
+    spawn(xPos, yPos) {
+        this.scene.EnemyPool.remove(this);
+        this.setActive(true);
+        this.setVisible(true);
+        this.setPosition(xPos, yPos);
+    }
+
+    die() {
+        this.scene.ActiveEnemies.killAndHide(this);
+        this.scene.ActiveEnemies.remove(this);
+        this.scene.EnemyPool.add(this);
+        //otras funcionalidades como 
+    }
+
+
 }
